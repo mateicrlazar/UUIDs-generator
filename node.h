b6f124823fc3ba4b3c96dd571ca0a02b771c0d5b
mateicrlazar <92542925+mateicrlazar@users.noreply.github.com>
2022-08-26 20:28:39 +0300

    Update node.h

--- node.h
+++ node.h
@@ -10,7 +10,7 @@ public:
 	// Helper functions
 	Node(unsigned short _node_if, unsigned long long _prefix); // The constructor will be called each time a node is created/initialized
 	void increase_prefix_by(unsigned how_many_times); // This function will help us emulate 100 000 requests per second for a node
-	unsigned long long prefix_getter(); // This function will help us access the prefix in order to store it in a text file for persistent storage
+	unsigned long long prefix_getter(); // This function will help us access the prefix in order to store it in nodes_data.txt for persistent storage
 	
 	// get_id implementation
 	unsigned long long get_id(); // The prototype of the function that needs to be implemented for this challenge!
